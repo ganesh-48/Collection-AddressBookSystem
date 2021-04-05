b3638ac038b7a49ae06ebe6f5cceacd414064b98
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-04-05 18:58:35 +0530

    ADDED: Added Edit contact on the persons name in contact details

--- src/main/java/com/addressbooksystem/AddressBook.java
+++ src/main/java/com/addressbooksystem/AddressBook.java
@@ -3,6 +3,7 @@ package com.addressbooksystem;
 /*
 *@Description - To create a contacts in address book with first name, last name, address, city, state, zip,mobile number.
 *Add new contact in address book
+* Edit contact using person name in address book
  */
 
 import java.util.Scanner;
@@ -38,6 +39,38 @@ public class AddressBook {
         System.out.println("contact added");
     }
 
+    //Edit person name
+    public void editContact(String firstName) {
+        if(contacts == null) {
+            System.out.println("Address book is empty");
+        } else {
+            if(contacts.getFirstName().equalsIgnoreCase(firstName)) {
+                System.out.println("Edit the Contact details:");
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter first name:");
+                contacts.setFirstName(scanner.next());
+                System.out.println("Enter last name:");
+                contacts.setLastName(scanner.next());
+                System.out.println("Enter the address:");
+                contacts.setAddress(scanner.next());
+                System.out.println("Enter city name:");
+                contacts.setCity(scanner.next());
+                System.out.println("Enter the state name:");
+                contacts.setState(scanner.next());
+                System.out.println("Enter the zip code:");
+                contacts.setZip(scanner.nextInt());
+                System.out.println("Enter the mobile number:");
+                contacts.setMobileNumber(scanner.nextLong());
+                System.out.println("Enter the email id:");
+                contacts.setEmail(scanner.next());
+
+                System.out.println("All contact of person name is updated");
+            } else {
+                System.out.println("Entered person name is not found in contacts");
+            }
+        }
+    }
+
     //print contact details
     public String toString() {
         return contacts.toString();
@@ -47,7 +80,11 @@ public class AddressBook {
         System.out.println("Welcome to address book problem");
 
         AddressBook addressBook = new AddressBook();
+        Scanner scanner = new Scanner(System.in);
+
         addressBook.addNewContact();
+        System.out.println("Enter the person name who's details want to edit :");
+        addressBook.editContact(scanner.next());
         System.out.println(addressBook.toString());
     }
 }
